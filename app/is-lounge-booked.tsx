eca29e41571953fd220d92f3293152f65adacaf3
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2025-12-27 01:37:42 -0800

    first commit

---
+++ app/is-lounge-booked.tsx
@@ -0,0 +1,66 @@
+import { parseIcsCalendar } from "@ts-ics/schema-zod";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+import { cacheLife } from "next/cache";
+import { IcsEvent, type IcsCalendar } from "ts-ics";
+import {
+  calendarID,
+  calendarScopeID,
+  LOUNGE_BOOKING_EVENT_PREFIX,
+} from "./constants";
+
+const calendarURL = `https://outlook.office365.com/owa/published/${calendarScopeID}/${calendarID}/calendar.ics`;
+dayjs.extend(timezone);
+dayjs.extend(utc);
+dayjs.tz.setDefault("America/Vancouver");
+
+type EventWithEndDate = Extract<IcsEvent, { end: { date: Date } }>;
+export async function IsLoungeBooked() {
+  "use cache";
+  cacheLife("hours");
+  const response = await fetch(calendarURL);
+  const data = await response.text();
+  const { events }: IcsCalendar = parseIcsCalendar(data);
+  if (!events) return null;
+  const now = dayjs();
+  const upcomingEvents = events.filter((event): event is EventWithEndDate => {
+    const eventDate = dayjs(event.start.date);
+    return (
+      event.summary.startsWith(LOUNGE_BOOKING_EVENT_PREFIX) &&
+      eventDate.isSame(now, "day") &&
+      eventDate.isAfter(now) &&
+      !!event.end
+    );
+  });
+  if (upcomingEvents.length === 0) {
+    return (
+      <p className="text-4xl font-bold text-green-500 animate-bounce [animation-duration:.4s]">
+        cвободно, fuck yeah!!!!
+      </p>
+    );
+  }
+
+  return (
+    <p className="text-3xl font-bold leading-normal">
+      NOOOO😭😭!! седня лаундж занят {formatEventsSummary(upcomingEvents)}
+      😡
+    </p>
+  );
+}
+const timeFormat = "h:mm A";
+function formatEventsSummary(events: EventWithEndDate[]) {
+  let str = "";
+  for (let i = 0; i < events.length; i++) {
+    const event = events[i];
+    str += `с ${dayjs(event.start.date).format(timeFormat)} до ${dayjs(
+      event.end.date,
+    ).format(timeFormat)}`;
+    if (i < events.length - 2) {
+      str += ", ";
+    } else if (i < events.length - 1) {
+      str += " и ";
+    }
+  }
+  return str;
+}
